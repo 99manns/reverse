c4a3f07bb0278d17b55f41195f3221ef67a17ec9
99manns <99manns@gmail.com>
2016-07-06 10:27:12 +0100

    Add atual cs file

---
+++ reverse/reverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name: ");
+            string name = Console.ReadLine();
+            string  reverse = "";
+            for (int i = name.Length-1; i > -1; i--)
+            {
+                reverse += name.ElementAt(i);
+            }
+            Console.WriteLine("Hi your name is: " + reverse);
+            Console.ReadKey();
+        }
+    }
+}
